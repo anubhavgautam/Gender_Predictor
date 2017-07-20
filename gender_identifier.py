c1549bb24074618b941b0d7e06acab69a07b2c41
anubhav-tcb2020 <anubhavgautam2020@gmail.com>
2017-07-20 21:41:19 +0530

    Initial Commit

---
+++ gender_identifier.py
@@ -0,0 +1,34 @@
+#! python3
+
+
+
+def gender_features(word):
+	return { 'last_letter' : word[-1] }
+#end
+
+ 
+
+ 
+import random
+import nltk 
+
+
+names = ([(name,'male') for name in ('D:\Project_files_notes\Gender-Identifier\male.txt')] + [(name,'female') for name in ('D:\Project_files_notes\Gender-Identifier\female.txt')])
+
+
+
+
+random.shuffle(names)
+featuresets = [(gender_features(n),g) for (n,g) in names]
+from nltk.classify import apply_features 
+train_set = apply_features(gender_features,names[86:])
+test_set = apply_features(gender_features, names[:85])
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+print classifier.classify(gender_features('ravi'))
+
+
+print nltk.classify.accuracy(classifier, test_set)
+
+classifier.show_most_informative_features(5)
+
